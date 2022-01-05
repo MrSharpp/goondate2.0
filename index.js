5dab9619e5ae66f1debb4e32f3af1cfe50317f4b
Amir Alam <sharpprogrammer2018@gmail.com>
2022-01-05 07:37:57 -0800

    update

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const http = require('http');
 const express = require('express');
-const router = express.Router();
+const app = express();
+const router = require('./route');
 
-http.createServer((req, res) => {
-   res.writeHead(200, { 'Content-Type': 'text/plain' });
-}).listen(3000);
 
-router.get('/', function(req, res){
-    res.send("HOME PAGE GO ON DATE");
+app.use('/', router);
+
+app.listen(3000, () => {
+
+    console.log('Server is up at localhost:3000...');
 });
-
-console.log('Server is up at localhost:3000...');
